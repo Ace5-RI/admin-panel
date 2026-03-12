d2b67fde496c236f571c0e570d9a2e422c12fe45
Ace5-RI <gedegrace25@gmail.com>
2026-03-12 16:42:15 +0800

    database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,10 +15,32 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-       $this->call([
-        UserSeeder::class,
+        $adminRole = Role::create(['name' => 'admin']);
+        $staffRole = Role::create(['name' => 'staff']);
+        $userRole = Role::create(['name' => 'user']);
+
+        $permissions = [
+            'create-post',
+            'edit-post',
+            'delete-post',
+            'view-post',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
+        $adminRole->syncPermissions($permissions);
+
+        $staffRole->syncPermissions(['create-post','edit-post','view-post']);
+
+       $user = User::create([
+        'name' => 'Admin',
+        'email' => 'admin@example.com',
+        'password' => bcrypt('password'),
        ]);
+       $user->assignRole('admin');
     }
 }
