943bce64da893fdf1942d4cfe8dfeb23b57d0313
Ace5-RI <gedegrace25@gmail.com>
2026-03-13 16:28:24 +0800

    database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,45 +2,96 @@
 
 namespace Database\Seeders;
 
+use App\Models\subscription;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
 
     /**
      * Seed the application's database.
      */
     public function run()
     {
-        $adminRole = Role::create(['name' => 'admin']);
-        $staffRole = Role::create(['name' => 'staff']);
-        $userRole = Role::create(['name' => 'user']);
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+        $staffRole = Role::firstOrCreate(['name' => 'staff']);
+        $userRole = Role::firstOrCreate(['name' => 'user']);
 
         $permissions = [
-            'create-post',
-            'edit-post',
-            'delete-post',
-            'view-post',
+            'view-clients',
+            'create-clients',
+            'edit-clients',
+            'delete-clients',
+
+            'view-subscriptions',
+            'create-subsctiptions',
+            'edit-subscriptions',
+            'delete-subsctiptions',
+
+            'view-payments',
+            'create-payments',
+            'edit-payments',
+            'delete-payments',
+
+            'export-reports',
+            'view-reports',
+
+            'manage-users',
+            'manage-roles',
         ];
 
+
         foreach ($permissions as $permission) {
-            Permission::create(['name' => $permission]);
+            Permission::firstOrCreate(['name' => $permission]);
         }
 
         $adminRole->syncPermissions($permissions);
 
-        $staffRole->syncPermissions(['create-post','edit-post','view-post']);
+        $staffRole->syncPermissions(['view-clients', 'edit-clients', 'view-subsscriptions', 'view-payments', 'export-reports', 'view-reports']);
 
-       $user = User::create([
-        'name' => 'Admin',
-        'email' => 'admin@example.com',
-        'password' => bcrypt('password'),
-       ]);
-       $user->assignRole('admin');
+        $admin = User::where('email', 'admin@example.com')->first();
+        if (!$admin) {
+            $user = User::create([
+                'name' => 'Admin',
+                'email' => 'admin@example.com',
+                'password' => bcrypt('password'),
+                'role' => 'admin',
+                'status' => 'active',
+            ]);
+        }
+
+        $user->assignRole('admin');
+
+         $admin = User::where('email', 'staff@example.com')->first();
+        if (!$admin) {
+            $user = User::create([
+                'name' => 'Staff',
+                'email' => 'staff@example.com',
+                'password' => bcrypt('password'),
+                'role' => 'staff',
+                'status' => 'active',
+            ]);
+        }
+
+        $user->assignRole('staff');
+
+         $admin = User::where('email', 'customer@example.com')->first();
+        if (!$admin) {
+            $user = User::create([
+                'name' => 'Customer',
+                'email' => 'customer@example.com',
+                'password' => bcrypt('password'),
+                'role' => 'customer',
+                'status' => 'active',
+            ]);
+        }
+
+        $user->assignRole('customer');
     }
 }
