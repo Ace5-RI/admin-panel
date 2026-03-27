a1cf0ef0ba3fffcae4f8e55d6cc1368d81d42e06
Ace5-RI <gedegrace25@gmail.com>
2026-03-27 23:50:20 +0800

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('langganan.login');
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\DashboardController;
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+});
+
+Route::middleware()->group(function() {
+    Route::post('/Logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('/dashboard', [AuthController::class, 'index'])->name('dashboard');
+    Route::post('/client', ClientController::class);
+    Route::post('/klien', [ClientController::class, 'index'])->name('klien.index');
 });
 
 Route::get('/dashboard', function () {
