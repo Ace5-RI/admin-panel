61b4229a5ed7b3bc42a2bb5778dd21d4b24febb5
Ace5-RI <gedegrace25@gmail.com>
2026-04-07 16:47:08 +0800

    dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -18,7 +18,9 @@ class DashboardController extends Controller
 
         $expiringsoon = Client::where('status','active')->whereBetween('subscription_end_date',[now(), now()->addDays(30)])->count();
         $totalrevenue = Client::where('status', 'paid')->sum('amount');
-
+        
+        $clientsLastMonth = Client::whereLastMonth('created_at', now()->subMonth()->month)->count();
+        $clientsPresent = $clientsLastMonth > 0 ? round(($totalClients - $clientsLastMonth) / $clientsLastMonth * 100, 2) : 0;
         $monthlyRevenue = Payment::where('status','paid')->whereYear('payment_date',now()->year)->select(
             DB::raw('MONTH(payment_date) as month' ),
             DB::raw('SUM(amount) as total')
