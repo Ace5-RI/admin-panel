0947d9323e2c5956648a6a4072ce676e41bdf819
Ace5-RI <gedegrace25@gmail.com>
2026-06-03 00:51:53 +0800

    fix: perbaiki migration subscription

--- database/migrations/2026_03_09_050923_subscription.php
+++ database/migrations/2026_03_09_050923_subscription.php
@@ -3,24 +3,18 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::dropIfExists('Subcription');
-        Schema::create('Subcription', function (Blueprint $table) {
+        Schema::dropIfExists('subscriptions');
+        Schema::create('subscriptions', function (Blueprint $table) {
             $table->id();
-
-            $table->foreignId('subcription_id')
-            ->constrained('subcription')
-            ->onDelete('cascade');
 
             // Informasi paket langganan
             $table->string('package_name');
-            $table->decimal('price', 15,2);
+            $table->decimal('price', 15, 2);
             $table->integer('duration_months');
 
             // Periode langganan
@@ -35,23 +29,18 @@ return new class extends Migration
                 'canceled',
             ])->default('inactive');
 
-            // Waktu dan penghapusan data langganan
+            // Waktu dan penghapusan data
             $table->timestamps();
             $table->softDeletes();
 
-            $table->index('subcription_id');
             $table->index('status');
             $table->index('end_date');
             $table->index(['start_date', 'end_date']);
-        }); 
-
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('subcription');
+        Schema::dropIfExists('subscriptions');
     }
 };
